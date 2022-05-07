fb529f5732b197e348a9a857e0e72d83b110b1d0
Jack Barrett <jbarrett1347@gmail.com>
2022-05-07 13:46:07 -0500

    updated styling for App to make styliing D.R.Y. & added form to gallery page

--- v0ncentartsite/src/components/aboutPage/AboutPage.jsx
+++ v0ncentartsite/src/components/aboutPage/AboutPage.jsx
@@ -1,52 +1,43 @@
 import { Box, Paper, Typography } from "@mui/material";
 
 // Importing needed components
-import MenuComponent from "../menuComponent/MenuComponent";
-import HeaderComponent from "../HeaderComponent/HeaderComponent";
+import TopNav from "../TopNav/TopNav";
 
 function AboutPage() {
   return (
-    <Box bgcolor="#000000" sx={{ p: 8 }}>
-      <Paper sx={{ p: 5 }}>
-        <HeaderComponent />
-        <MenuComponent />
-        <Typography variant="h2" textAlign={"center"} sx={{ p: 4 }}>
-          About V0ncent
+    <>
+      <TopNav />
+      <Typography variant="h2" textAlign={"center"} sx={{ p: 4 }}>
+        About V0ncent
+      </Typography>
+      <Box sx={{ p: 4 }} textAlign={"left"}>
+        <Typography variant="h4">
+          {/* TODO: add a image for self portrait next to bio */}
+          Hello! My name is Vincent Banks, I am a self taught artist and
+          computer programmer. I enjoy creating grotesque style pieces and
+          comics. You can check out my other links below to see more of my work!
+          <br />
+          I hope this site finds you well and you have enjoyed a platform to
+          share your work!
+          <br />
+          <br />
+          GitHub:{" "}
+          <a href="https://github.com/v0ncent" target="_blank" rel="noreferrer">
+            v0ncent
+          </a>
+          <br />
+          Instagram:{" "}
+          <a
+            href="https://instagram.com/v0ncent?igshid=YmMyMTA2M2Y="
+            target="_blank"
+            rel="noreferrer"
+          >
+            @v0ncent
+          </a>
+          <br />
         </Typography>
-        <Box sx={{ p: 4 }} textAlign={"left"}>
-          <Typography variant="h4">
-            {/* TODO: add a image for self portrait next to bio */}
-            Hello! My name is Vincent Banks, I am a self taught artist and
-            computer programmer. I enjoy creating grotesque style pieces and
-            comics. You can check out my other links below to see more of my
-            work!
-            <br />
-            I hope this site finds you well and you have enjoyed a platform to
-            share your work!
-            <br />
-            <br />
-            GitHub:{" "}
-            <a
-              href="https://github.com/v0ncent"
-              target="_blank"
-              rel="noreferrer"
-            >
-              v0ncent
-            </a>
-            <br />
-            Instagram:{" "}
-            <a
-              href="https://instagram.com/v0ncent?igshid=YmMyMTA2M2Y="
-              target="_blank"
-              rel="noreferrer"
-            >
-              @v0ncent
-            </a>
-            <br />
-          </Typography>
-        </Box>
-      </Paper>
-    </Box>
+      </Box>
+    </>
   );
 }
 
--- v0ncentartsite/src/components/galleryPage/GalleryPage.jsx
+++ v0ncentartsite/src/components/galleryPage/GalleryPage.jsx
@@ -1,20 +1,47 @@
-import { Box, Paper, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
+import { Box, Grid, TextField, Button } from "@mui/material";
 
 // Importing needed components
-import MenuComponent from "../menuComponent/MenuComponent";
-import HeaderComponent from "../HeaderComponent/HeaderComponent";
+import TopNav from "../TopNav/TopNav";
+
+import { styled } from "@mui/material/styles";
+const Input = styled("input")({
+  display: "none",
+});
 
 
 function GalleryPage() {
   return (
-    <Box bgcolor="#000000" sx={{ p: 8 }}>
-      <Paper sx={{ p: 5 }}>
-        <HeaderComponent />
-        <MenuComponent />
-        {/* TODO: add head title for gallery page called "The Gallery" (ill draw this) */}
-        <Typography>galleryPage</Typography>
-      </Paper>
-    </Box>
+    <>
+      <TopNav />
+      {/* TODO: add head title for gallery page called "The Gallery" (ill draw this) */}
+      <Grid container sx={{ p: 4 }}>
+        <Box component={"form"}>
+          <Grid item xs={12}>
+            <Typography>upload & title your work here</Typography>
+            <TextField label="Title" variant="outlined" />
+            <br />
+            <br />
+            <Button variant="contained" color="secondary">
+              submit
+            </Button>
+            <br />
+            <br />
+            <label htmlFor="contained-button-file">
+              <Input
+                accept="image/*"
+                id="contained-button-file"
+                multiple
+                type="file"
+              />
+              <Button variant="contained" component="span">
+                Upload
+              </Button>
+            </label>
+          </Grid>
+        </Box>
+      </Grid>
+    </>
   );
 }
 
