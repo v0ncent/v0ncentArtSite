d48e95333c0fdadff0813235c3011ff08faeb04b
Jack Barrett <jbarrett1347@gmail.com>
2022-05-10 12:44:26 -0500

    added state tracking to title in galleryPage

--- v0ncentartsite/src/components/galleryPage/GalleryPage.jsx
+++ v0ncentartsite/src/components/galleryPage/GalleryPage.jsx
@@ -1,15 +1,19 @@
+// Importing React & site nav component
+import { useState } from "react";
+import TopNav from "../TopNav/TopNav";
+
+// Importing needed MUI components
 import {
+  Box,
+  Grid,
+  Button,
   Accordion,
+  TextField,
   Typography,
   AccordionSummary,
   AccordionDetails,
 } from "@mui/material";
-import { Box, Grid, TextField, Button } from "@mui/material";
-
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-
-// Importing needed components
-import TopNav from "../TopNav/TopNav";
 
 import { styled } from "@mui/material/styles";
 const Input = styled("input")({
@@ -17,6 +21,22 @@ const Input = styled("input")({
 });
 
 function GalleryPage() {
+  const [uploadForm, setUploadForm] = useState({
+    title: "",
+    imgURL: "",
+  });
+
+  // function updateUploadForm(value) {
+  //   return setUploadForm((prev) => {
+  //     return { ...prev, ...value };
+  //   });
+  // }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    console.log("poop");
+  }
+
   return (
     <>
       {/* TODO: add head title for gallery page called "The Gallery" (ill draw this) */}
@@ -42,14 +62,24 @@ function GalleryPage() {
           <AccordionDetails>
             <Typography>upload & title your work here</Typography>
             <Grid item xs={12}>
-              <Box component={"form"}>
-                <TextField label="title" variant="outlined" />
+              <Box component={"form"} onSubmit={onSubmit}>
+                <TextField
+                  id={"title"}
+                  label="title"
+                  variant="outlined"
+                  value={uploadForm.title}
+                  onChange={(e) => setUploadForm({ title: e.target.value })}
+                />
                 <br />
                 <br />
-                <Button variant="contained" color="secondary">
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  id="submit"
+                  type="submit"
+                >
                   submit
-                </Button>
-                {"  .  "}
+                </Button>{" "}
                 <label htmlFor="contained-button-file">
                   <Input
                     accept="image/*"
@@ -57,7 +87,14 @@ function GalleryPage() {
                     multiple
                     type="file"
                   />
-                  <Button variant="contained" component="span">
+                  <Button
+                    id="imgURL"
+                    type="file"
+                    component="span"
+                    variant="contained"
+                    value={uploadForm.imgURL}
+                    onChange={(e) => setUploadForm({ imgURL: e.target.value })}
+                  >
                     Upload
                   </Button>
                 </label>
@@ -65,6 +102,10 @@ function GalleryPage() {
             </Grid>
           </AccordionDetails>
         </Accordion>
+        <Grid item xs={12}>
+          <Typography>{uploadForm.title}</Typography>
+          <Typography>{uploadForm.imgURL}</Typography>
+        </Grid>
       </Grid>
     </>
   );
