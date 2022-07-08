f3476e03a4a0f061c4bf0af87e5cd0f2d4317bb8
v0ncent <83373495+v0ncent@users.noreply.github.com>
2022-07-07 21:23:05 -0500

    created multiple routes for subsections of site

--- v0ncentartsite/server/server.js
+++ v0ncentartsite/server/server.js
@@ -5,11 +5,22 @@ require("dotenv").config({ path: "../config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/galleryRoutes"));
 // get driver connection
 const dbo = require("./db/conn");
 
+//set up new routes for app to use
+const router = express.Router();
+
+
+//Function for building all routes
+function buildRoutes(){
+  //galleryPage
+  app.use(require("./routes/galleryRoutes"));
+
+}
+
 app.listen(port, () => {
+  buildRoutes(); //build express routes
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
