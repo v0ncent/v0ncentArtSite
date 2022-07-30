cc6531e655f12e13ffca501f3f2c2097620dd58f
v0ncent <83373495+v0ncent@users.noreply.github.com>
2022-07-30 17:09:49 -0500

    Added Config.java, created .env file for server, created Constants class

---
+++ v0ncentartsite/server/src/main/java/C.java
@@ -0,0 +1,8 @@
+/*
+*----------------
+*  Server Constants Class
+* ---------------
+* This class holds all Constant variables to be accessed across project
+*/
+public final class C {
+}
--- v0ncentartsite/server/src/main/java/Main.java
+++ v0ncentartsite/server/src/main/java/Main.java
@@ -9,6 +9,6 @@ public class Main{
     }
     public static void main(String[] args){
         onReady();
-        System.out.println("Hello Cunts!");
+        System.out.println(Config.get("TEST"));
     }
 }
