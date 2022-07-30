022bee38f8b582abb30a79bdcbb872f99ec77217
v0ncent <83373495+v0ncent@users.noreply.github.com>
2022-07-29 21:57:33 -0500

    Began backend Overhaul,added Server package which houses Java project

--- v0ncentartsite/src/components/comicsPage/ComicsPage.jsx
+++ v0ncentartsite/src/components/comicsPage/ComicsPage.jsx
@@ -2,8 +2,21 @@ import { Typography } from "@mui/material";
 
 // Importing needed components
 import TopNav from "../TopNav/TopNav";
+import { 
+  getAllItems,  
+  //these arent used but they are there to be used
+  getOneItem,
+  updateOneItem,
+  deleteOneItem
+} from "../../MongoGetter";
 
 function ComicsPage() {
+  //comics route test
+
+
+
+
+
   return (
     <>
       <TopNav />
