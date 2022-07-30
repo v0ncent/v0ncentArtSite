095d32a48ad2fd544b11c54b2aade474ec4399b0
v0ncent <83373495+v0ncent@users.noreply.github.com>
2022-07-29 23:15:44 -0500

    Added slf4j Logger to Java Backend and ran test script

--- v0ncentartsite/src/main/java/Main.java
+++ v0ncentartsite/src/main/java/Main.java
@@ -1,7 +1,14 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /*
 * */
-public class Main {
+public class Main{
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    static void onReady(){
+        LOGGER.info("Test!");
+    }
     public static void main(String[] args){
+        onReady();
         System.out.println("Hello Cunts!");
     }
 }
