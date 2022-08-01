5a0fa2e75c0ac3f8782bc78b58e4f5cd8a78b444
v0ncent <83373495+v0ncent@users.noreply.github.com>
2022-08-01 00:21:38 -0500

    Created mongoDB Connection, created Gallery.java, created ItemRepository.java

--- v0ncentartsite/server/src/main/java/com/v0ncent/server/Repository/ItemRepository.java
+++ v0ncentartsite/server/src/main/java/com/v0ncent/server/Repository/ItemRepository.java
@@ -1,11 +1,15 @@
+/*
+ * --------------------------------------------------------------------------
+ *                   Server Application ItemRepository Interface
+ * This creates all methods to be interfaced for querying MONGODB
+ * @author v0ncent
+ * --------------------------------------------------------------------------
+ */
 package com.v0ncent.server.Repository;
-
 import com.v0ncent.server.POJO.Gallery;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-
 import java.util.List;
-
 public interface ItemRepository extends MongoRepository<Gallery,String> {
     @Query("{name:'?0'")
     Gallery findByNameAndTitle(String name, String title);
@@ -13,6 +17,6 @@ public interface ItemRepository extends MongoRepository<Gallery,String> {
     @Query(value="{id:'?0'}", fields = "{'name' : 1, 'title' : 1, 'imageURL' : 1, 'datePosted' : 1}")
     List<Gallery> findAll(String id);
 
-    public long count();
+    long count();
 
 }
