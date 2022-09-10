07151a8111997077a5b1725e1b571484a17bcc7d
v0ncent <banksvincent5@gmail.com>
2022-09-10 06:37:49 -0500

    Documented GalleryMongo Functions

--- v0ncentartsite/src/components/galleryPage/GalleryMongo.ts
+++ v0ncentartsite/src/components/galleryPage/GalleryMongo.ts
@@ -5,6 +5,10 @@ const GET_ONE_ROUTE:string = api + "/getOneGallery/"
 const CREATE_LISTING_ROUTE:string = api + "/createListingGallery";
 const UPDATE_ONE_ROUTE:string = api + "/updateOneGallery/"
 const DELETE_ONE_ROUTE:string = api + "/deleteOneGallery/"
+/**
+ * Retrieves all documents from DB.
+ * @returns returns a list of all documents as objects.
+ */
 const getAll = async () => {
         let all = []
         await fetch(GET_ALL_ROUTE)
@@ -12,6 +16,11 @@ const getAll = async () => {
         .then(data => all.push(data))
         return all
 }
+/**
+ * Gets a single document from DB.
+ * @param id id of document to query.
+ * @returns returns a query as object.
+ */
 const getOne = async (id:number) => {
         let one = ''
         await fetch(GET_ONE_ROUTE + id)
@@ -19,6 +28,10 @@ const getOne = async (id:number) => {
         .then (data =>  one = data)
         return one
 }
+/**
+ * Deletes one Document from DB.
+ * @param id id of document to delete.
+ */
 const deleteOne = async (id:number) => {
         try {
                await fetch(DELETE_ONE_ROUTE+id,{
